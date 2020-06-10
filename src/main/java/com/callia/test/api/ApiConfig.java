631f67ce43fde2d63f2648b6311c0edabc41c71c
Seth Fiawoo <syfiawoo@gmail.com>
2020-06-10 15:27:43 -0500

    Modify Swagger Implementation

--- src/main/java/com/callia/test/api/ApiConfig.java
+++ src/main/java/com/callia/test/api/ApiConfig.java
@@ -3,24 +3,29 @@ package com.callia.test.api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 public class ApiConfig {
     @Bean
     public Docket docket(ApiInfo apiInfo){
         return  new Docket(DocumentationType.SWAGGER_2)
-                .groupName("events-api")
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo)
-                .select().paths(regex("/api/*"))
-                .build();
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.callia.test.controller"))
+                .paths(PathSelectors.ant("/*"))
+                .build()
+                .apiInfo(apiInfo);
+//                .groupName("events-api")
+//                .useDefaultResponseMessages(false)
+//                .apiInfo(apiInfo)
+//                .select().paths(regex("/api/*"))
+//                .build();
     }
 
     @Bean
@@ -28,7 +33,7 @@ public class ApiConfig {
         return new ApiInfoBuilder()
                 .title("Events Api")
                 .description("Api for events relating to customer order")
-                .version("0.0.1")
+                .version("1.0")
                 .build();
     }
 
